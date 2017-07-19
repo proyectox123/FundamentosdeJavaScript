3738c5acbad3e8fbee8b1317b13d37a05254a125
MHO <mauricio.hernandz@ironbit.com.mx>
2017-07-19 18:22:05 -0500

    Definiendo la clase Punto - Object.create en JavaScript

--- distance_between_two_points.js
+++ distance_between_two_points.js
@@ -1,19 +1,40 @@
-function Point(x, y){
-	this.x = x
-	this.y = y
+// function Point(x, y){
+// 	this.x = x
+// 	this.y = y
+// }
+
+// Point.prototype.moveInX = function moveInX(x) { this.x += x },
+// Point.prototype.moveInY = function moveInY(y) { this.y += y },
+// Point.prototype.distance = function distance(p){
+// 	const x = this.x - p.x
+// 	const y = this.y - p.y
+
+// 	return Math.sqrt(x * x + y * y)
+// }
+
+//const p1 = new Point(0, 4)
+//const p2 = new Point(3, 0)
+
+const Point = {
+	init: function(x, y) {
+		this.x = x
+		this.y = y
+	},
+	moveInX: function moveInX(x) { this.x += x },
+	moveInY: function moveInY(y) { this.y += y },
+	distance: function distance(p) {
+		const x = this.x - p.x
+		const y = this.y - p.y
+
+		return Math.sqrt(x * x + y * y)
+ 	}
 }
 
-Point.prototype.moveInX = function moveInX(x) { this.x += x },
-Point.prototype.moveInY = function moveInY(y) { this.y += y },
-Point.prototype.distance = function distance(p){
-	const x = this.x - p.x
-	const y = this.y - p.y
+const p1 = Object.create(Point)
+const p2 = Object.create(Point)
 
-	return Math.sqrt(x * x + y * y)
-}
-
-const p1 = new Point(0, 4)
-const p2 = new Point(3, 0)
+p1.init(0, 4)
+p2.init(3, 0)
 
 console.log(p1.distance(p2))
 console.log(p2.distance(p1))
