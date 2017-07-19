51ba8759f2e9e132cc2c8a95e814a40cb399572e
MHO <mauricio.hernandz@ironbit.com.mx>
2017-07-19 17:37:23 -0500

    Agrega métodos para mover los puntos - Objetos Avanzado en JavaScript

--- distance_between_two_points.js
+++ distance_between_two_points.js
@@ -1,11 +1,15 @@
 const p1 = {
 	x : 0,
-	y : 4
+	y : 4,
+	moveInX : function (x) { this.x += x },
+	moveInY : function (y) { this.y += y }
 }
 
 const p2 = {
 	x : 3,
-	y : 0
+	y : 0,
+	moveInX : function (x) { this.x += x },
+	moveInY : function (y) { this.y += y }
 }
 
 const getX = () => p1.x - p2.x
@@ -13,3 +17,4 @@ const getY = () => p1.y - p2.y
 const distance = (p1, p2) => Math.sqrt(getX() * getX() + getY() * getY())
 
 console.log(distance(p1, p2))
+console.log(p1.moveInX(10))
