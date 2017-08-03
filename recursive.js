2d2a4d846a82a7d692096f537edf26a025f900e0
MHO <mauricio.hernandz@ironbit.com.mx>
2017-08-03 15:40:32 -0500

    Funciones Recursivas

---
+++ recursive.js
@@ -0,0 +1,20 @@
+const fibonacci = num => {
+	if(num == 1){
+		return 0;
+	}
+
+	if(num == 2){
+		return 1;
+	}
+
+	return fibonacci(num - 1) + fibonacci(num - 2) 
+}
+
+fibonacci(1)//0
+fibonacci(2)//1
+fibonacci(3)//1
+fibonacci(4)//2
+fibonacci(5)//3
+fibonacci(6)//5
+fibonacci(7)//8
+fibonacci(8)//13
